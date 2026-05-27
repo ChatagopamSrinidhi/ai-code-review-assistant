9ec9195f050c1f77db07454c65e040d86806cfc5
srinidhi <chatagopamsrinidhi459@gmail.com>
2026-05-27 12:31:35 +0530

    fixed requirements

--- backend/app.py
+++ backend/app.py
@@ -7,10 +7,27 @@ load_dotenv()
 
 from routes.review import review_bp
 from routes.auth import auth_bp
+from models import db
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/api/*": {"origins": "https://ai-code-review-assistant-xi.vercel.app"}})
+# CORS
+CORS(app, resources={
+    r"/api/*": {
+        "origins": "https://ai-code-review-assistant-xi.vercel.app"
+    }
+})
+
+# DATABASE CONFIG
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# INITIALIZE DB
+db.init_app(app)
+
+# CREATE TABLES
+with app.app_context():
+    db.create_all()
 
 # REGISTER ROUTES
 app.register_blueprint(review_bp, url_prefix="/api/review")
