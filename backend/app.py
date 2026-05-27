dd400a48e09840f8eb4c3d127a3e9d52889bf393
srinidhi <chatagopamsrinidhi459@gmail.com>
2026-05-27 11:53:41 +0530

    fixed cors

--- backend/app.py
+++ backend/app.py
@@ -6,15 +6,15 @@ import os
 load_dotenv()
 
 from routes.review import review_bp
-from routes.auth import auth_bp   # ✅ ADD THIS
+from routes.auth import auth_bp
 
 app = Flask(__name__)
 
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "https://ai-code-review-assistant-xi.vercel.app"}})
 
 # REGISTER ROUTES
 app.register_blueprint(review_bp, url_prefix="/api/review")
-app.register_blueprint(auth_bp, url_prefix="/api/auth")  # ✅ ADD THIS
+app.register_blueprint(auth_bp, url_prefix="/api/auth")
 
 @app.route("/")
 def home():
